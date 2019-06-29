64f7c3174c8ad5065995ce85eda1bcaf1010f033
Nelson <37249915+nelsob44@users.noreply.github.com>
2019-06-29 15:43:27 +0100

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-project', 'root', '', {
+const sequelize = new Sequelize('node-project', 'root', 'Mi201992', {
     dialect: 'mysql', 
     host: 'localhost'
 });
